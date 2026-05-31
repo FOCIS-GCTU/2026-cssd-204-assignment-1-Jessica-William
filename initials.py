0e455a0cc1bd449b39eb13137dbedc1eea0180f9
Jessica-William <jessica18william@gmail.com>
2026-05-31 22:59:41 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Program to display my iniials in stylized large block letters.
+# Assignment Number: 2
+#
+# Name: Jessica William
+# STUDENT ID:  2425403937
+# Email: 2425403937@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Jessica William, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
 
+    # The format of the output shall be
+
+    print("...JJW")
+   
+    # Displaying my initial in a larger format
+
+    print("...JJJJJJJJJJJJ.......JJJJJJJJJJJJ.......WW........WW.......")
+    print(".......JJJJ...............JJJJ...........WW........WW.......")
+    print(".......JJJJ...............JJJJ...........WW........WW.......")
+    print(".......JJJJ...............JJJJ...........WW...WW...WW.......")
+    print(".......JJJJ...............JJJJ...........WW...WW...WW.......")
+    print(".......JJJJ...............JJJJ...........WW...WW...WW.......")
+    print("...JJ..JJJJ...........JJ..JJJJ...........WW.WW.WW..WW.......")
+    print("...JJ..JJJJ...........JJ..JJJJ...........WW.WW..WW.WW.......")
+    print("...JJJJJJJ............JJJJJJJ............WWWW....WWWW.......")
+    print("....JJJJJJ.....****....JJJJJJ.....****....WWW.....WWW****...")
+    print()
 
 
 main()
+#call of the main function
+
+#end of my program .
